7d021d0b30a30369dd5eb0bee1f2d92012af9181
furball514 <karthik.nallasivan@gmail.com>
2017-09-17 18:04:09 +0530

    6:04

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <Fabric style={styles.container}>
+        <div>
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          href={`${process.env.PUBLIC_URL}/old portfolio/oldportfolio.html`}
-          target="_blank"
-        >
-          click here
-        </a>
-      </div>
+        <DefaultButton>
+          <a href={`${process.env.PUBLIC_URL}/old portfolio/oldportfolio.html`}>click here</a>
+        </DefaultButton>
+      </Fabric>
     );
   }
 }
 
-export default App;
+const styles = {
+  container: {
+    backgroundColor: 'white',
+    fontFamily: 'Arial',
+  },
+};
+
+//no fonts // no icons
