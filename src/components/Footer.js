a51d3961bb2a5b85cde5f269f62a67fcb08bbc0d
furball514 <karthik.nallasivan@gmail.com>
2017-09-21 20:46:00 +0530

    8:45

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,4 +5,19 @@ const Footer = () => <footer className={css(styles.footer)} />;
 
 export default Footer;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  footer: {
+    background: '#000',
+    color: '#fff',
+    fontWeight: '400',
+    fontSize: '14px',
+    padding: '32px 3%',
+    position: 'relative',
+    width: '94%',
+    minWidth: '320px', //
+    '@media (max-width: 320px)': {
+      width: '320px',
+    },
+    margin: '0 auto',
+  },
+});
