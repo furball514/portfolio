0feb0d5352a4c04673fc37b4e1c8f57a9002a5f1
furball514 <karthik.nallasivan@gmail.com>
2017-09-22 15:58:32 +0530

    3:58

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import { StyleSheet, css } from 'aphrodite';
+import 'font-awesome/css/font-awesome.css';
+import cert from '../fcccert.jpg';
 
 const LeftFooter = () => (
-  <ul className={css(styles.leftFooter)}>
-    <li>
-      <span />
-      <a />
-    </li>
-    <li>
-      <span />
-      <a />
-    </li>
-  </ul>
+  <div className={css(styles.leftFooter)}>
+    <ul className={css(styles.linkList)}>
+      <li className={css(styles.heading)}>Links</li>
+      <li>
+        <a href="https://github.com/furball514" className={css(styles.link)}>
+          <span className="fa fa-github" aria-hidden />&nbsp; GitHub
+        </a>
+      </li>
+      <li>
+        <a href="http://scr.im/3nji" target="_blank" className={css(styles.link)}>
+          <span className="fa fa-envelope" aria-hidden />&nbsp; Mail
+        </a>
+      </li>
+      <li>
+        <a
+          href={`${process.env.PUBLIC_URL}/old portfolio/oldportfolio.html`}
+          className={css(styles.link)}>
+          <span className="fa fa-code" aria-hidden />&nbsp; Old Portfolio
+        </a>
+      </li>
+    </ul>
+  </div>
 );
 
 const RightFooter = () => (
   <div className={css(styles.rightFooter)}>
-    <img /> <a />
+    <div className={css(styles.imageContainer)}>
+      <a href="https://www.freecodecamp.org/furball514/front-end-certification">
+        <img src={cert} alt="frontend certificate" className={css(styles.image)} />
+      </a>
+      <a href="https://github.com/furball514/portfolio" className={css(styles.source)}>
+        <span className="fa fa-code-fork" aria-hidden />&nbsp; View This Site On GitHub
+      </a>
+    </div>
   </div>
 );
 
@@ -55,22 +76,42 @@ const styles = StyleSheet.create({
     '@media (max-width: 319px)': {
       width: '319px',
     },
-    display: 'flex',
-    alignItems: 'center',
-    '@media (max-width: 480px)': {
-      flexWrap: 'wrap',
-    },
+    '@media (max-width: 480px)': {},
   },
   leftFooter: {
     width: '50%',
-    listStyleType: 'none',
+    float: 'left',
   },
   rightFooter: {
     width: '50%',
+    float: 'right',
   },
   footerText: {
     textAlign: 'center',
     color: '#666',
+    fontWeight: 'bold',
+  },
+  linkList: {
+    listStyleType: 'none',
+  },
+  link: {
+    textDecoration: 'none',
+    color: '#fff',
+  },
+  image: {
+    width: '45%',
+    height: '20vw',
+  },
+  imageContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  source: {
+    textAlign: 'right',
+    textDecoration: 'none',
+    color: '#fff',
+  },
+  heading: {
     fontWeight: 'bold',
   },
 });
