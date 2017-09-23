ac991e818992347689276aed6562c273cb0ee98d
furball514 <karthik.nallasivan@gmail.com>
2017-09-23 17:22:56 +0530

    5:22

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import { Link } from 'office-ui-fabric-react/lib/Link';
+import { StyleSheet, css } from 'aphrodite';
 
 const NavBar = () => (
-  <div>
-    <h1 style={{ fontSize: '76px' }}>Portfolio</h1>
-    <nav>
-      <ul style={{ listStyleType: 'none' }}>
-        <li>
-          <Link>About</Link>
-        </li>
-        <li>
-          <Link>Skills</Link>
-        </li>
-        <li>
-          <Link>Showcase</Link>
-        </li>
-        <li>
-          <Link>Tools</Link>
-        </li>
-      </ul>
-    </nav>
+  <div className={css(styles.navbar)}>
+    <div style={{ backgroundColor: '#5c005c' }}>
+      <h1 className={css(styles.h1)}>Portfolio</h1>
+      <nav>
+        <ul className={css(styles.ul)}>
+          <li>
+            <a className={css(styles.link)} href="">
+              About
+            </a>
+          </li>
+          <li>
+            <a className={css(styles.link)} href="">
+              Skills
+            </a>
+          </li>
+          <li>
+            <a className={css(styles.link)} href="">
+              Showcase
+            </a>
+          </li>
+          <li>
+            <a className={css(styles.link)} href="">
+              Tools
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </div>
 );
 
 export default NavBar;
+
+const styles = StyleSheet.create({
+  navbar: {
+    zIndex: 500,
+    position: 'relative',
+  },
+  h1: {
+    fontSize: '76px',
+    fontFamily: 'Source Sans Pro ExtraLight',
+    color: '#f8f8f8',
+  },
+  ul: {
+    listStyleType: 'none',
+    display: 'flex',
+    justifyContent: 'space-around',
+  },
+  link: {
+    fontSize: '17px',
+    fontWeight: '300',
+    textDecoration: 'none',
+    color: '#fff',
+    ':hover': {
+      fontWeight: '700',
+    },
+  },
+});
