0f106c49fd5ac698536dc61c51c2d74bc80feb38
furball514 <karthik.nallasivan@gmail.com>
2017-09-28 20:57:26 +0530

    8:57

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,85 +17,93 @@ export default class NavBar extends Component {
 
   render() {
     return (
-      <div className={css(styles.navbar)}>
-        <div className={css(this.state.fixed ? styles.fixedDiv : styles.div)}>
-          <h1 className={css(this.state.fixed ? styles.fixedH1 : styles.h1)}>Portfolio</h1>
-          <nav className={css(styles.linksContainer)}>
-            <ul className={css(styles.ul)}>
-              <li>
-                <a className={css(styles.link)} href="">
-                  About
-                </a>
-              </li>
-              <li>
-                <a className={css(styles.link)} href="">
-                  Skills
-                </a>
-              </li>
-              <li>
-                <a className={css(styles.link)} href="">
-                  Showcase
-                </a>
-              </li>
-              <li>
-                <a className={css(styles.link)} href="">
-                  Tools
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
+      <div className={css(styles.pageTitle)}>
+        <h1 className={css(styles.heading)}>Portfolio</h1>
+        <nav>
+          <ul className={css(styles.list)}>
+            <li className={css(styles.listItem)}>
+              <a className={css(styles.link)} href="" title="About">
+                About
+              </a>
+            </li>
+            <li className={css(styles.listItem)}>
+              <a className={css(styles.link)} href="" title="Skills">
+                Skills
+              </a>
+            </li>
+            <li className={css(styles.listItem)}>
+              <a className={css(styles.link)} href="" title="Showcase">
+                Showcase
+              </a>
+            </li>
+            <li className={css(styles.listItem)}>
+              <a className={css(styles.link)} href="" title="Tools">
+                Tools
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  navbar: {
+  pageTitle: {
     color: 'white',
     backgroundColor: '#5c005c',
     padding: '0 5%',
-  },
-  div: {
     height: '236px',
   },
-  h1: {
+  heading: {
     fontSize: '76px',
     fontFamily: 'Source Sans Pro ExtraLight',
     color: '#f8f8f8',
+    transition: 'font-size 167ms cubic-bezier(.1,.9,.2,1)',
     paddingTop: '50px',
+  },
+  list: {
+    listStyleType: 'none',
+    margin: 0,
+    padding: 0,
+    overflow: 'hidden',
+  },
+  listItem: {
+    float: 'left',
+  },
+  link: {
+    display: 'block',
+    textAlign: 'center',
+    padding: '14px 1.5vw',
+    textDecoration: 'none',
+    fontFamily: 'Source Sans Pro ExtraLight',
+    fontSize: '17px',
+    color: '#fff',
+    fontWeight: 'bold',
+    ':hover': {
+      fontFamily: 'Source Sans Pro',
+    },
+    '::after': {
+      display: 'block',
+      content: 'attr(title)',
+      fontFamily: 'Source Sans Pro',
+      fontWeight: '100',
+      height: '0',
+      overflow: 'hidden',
+      visibility: 'hidden',
+    },
+  },
+});
+
+/*
     '@media (max-width: 480px)': {
       fontSize: '48px',
       paddingTop: '40px',
-    },
-    transition: 'font-size 167ms cubic-bezier(.1,.9,.2,1)',
-  },
-  linksContainer: {},
-  ul: {
-    listStyleType: 'none',
-    display: 'flex',
-  },
-  link: {
-    fontFamily: 'Source Sans Pro ExtraLight',
-    fontSize: '17px',
-    fontWeight: 'bold',
-    textDecoration: 'none',
-    color: '#fff',
-    ':hover': {
-      fontFamily: 'Source Sans Pro',
-      fontWeight: '900',
-    },
-  },
-  /* fixed */
-  fixedDiv: {
     boxShadow: '0 10px 30px -10px rgba(0,0,0,.2)',
     position: 'fixed',
     zIndex: 500,
     height: '128px',
-  },
-  fixedH1: {
     fontSize: '24px',
     paddingTop: '32px',
     color: '#f8f8f8',
-  },
-});
+*/
