c04731a0de5ec6a14c92075e860afce0ae832255
furball514 <karthik.nallasivan@gmail.com>
2017-09-30 15:45:38 +0530

    3:45

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,32 +15,31 @@ export default class NavBar extends Component {
     this.setState({ fixed });
   }
 
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll.bind(this));
+  }
+
   render() {
+    const unfixed = !this.state.fixed;
+    const smDevice = window.matchMedia('screen and (max-width: 480px)').matches;
+    const pageTitle = css(unfixed || smDevice ? styles.pageTitle : styles.fixedPageTitle);
+    const heading = css(unfixed || smDevice ? styles.heading : styles.fixedHeading);
+    /* jsx */
     return (
-      <div className={css(styles.pageTitle)}>
-        <h1 className={css(styles.heading)}>Portfolio</h1>
+      <div className={pageTitle}>
+        <h1 className={heading}>Portfolio</h1>
         <nav>
           <ul className={css(styles.list)}>
-            <li className={css(styles.listItem)}>
-              <a className={css(styles.link)} href="" title="About">
-                About
-              </a>
-            </li>
-            <li className={css(styles.listItem)}>
-              <a className={css(styles.link)} href="" title="Skills">
-                Skills
-              </a>
-            </li>
-            <li className={css(styles.listItem)}>
-              <a className={css(styles.link)} href="" title="Showcase">
-                Showcase
-              </a>
-            </li>
-            <li className={css(styles.listItem)}>
-              <a className={css(styles.link)} href="" title="Tools">
-                Tools
-              </a>
-            </li>
+            {navLinks.map(navLink => (
+              <li className={css(styles.listItem)}>
+                <a
+                  className={css(unfixed || smDevice ? styles.link : styles.fixedLink)}
+                  href={navLink.href}
+                  title={navLink.title}>
+                  {navLink.title}
+                </a>
+              </li>
+            ))}
           </ul>
         </nav>
       </div>
@@ -48,12 +47,22 @@ export default class NavBar extends Component {
   }
 }
 
+const navLinks = [
+  { href: '#about', title: 'About' },
+  { href: '#skills', title: 'Skills' },
+  { href: '#showcase', title: 'Showcase' },
+  { href: '#tools', title: 'Tools' },
+];
+
 const styles = StyleSheet.create({
   pageTitle: {
     color: 'white',
     backgroundColor: '#5c005c',
     padding: '0 5%',
     height: '236px',
+    '@media (max-width: 480px)': {
+      height: '200px',
+    },
   },
   heading: {
     fontSize: '76px',
@@ -61,6 +70,10 @@ const styles = StyleSheet.create({
     color: '#f8f8f8',
     transition: 'font-size 167ms cubic-bezier(.1,.9,.2,1)',
     paddingTop: '50px',
+    '@media (max-width: 480px)': {
+      fontSize: '48px',
+      paddingTop: '40px',
+    },
   },
   list: {
     listStyleType: 'none',
@@ -92,18 +105,51 @@ const styles = StyleSheet.create({
       overflow: 'hidden',
       visibility: 'hidden',
     },
-  },
-});
-
-/*
     '@media (max-width: 480px)': {
-      fontSize: '48px',
-      paddingTop: '40px',
-    boxShadow: '0 10px 30px -10px rgba(0,0,0,.2)',
+      fontSize: '15px',
+    },
+  },
+  /* fixed */
+  fixedPageTitle: {
+    color: 'white',
+    backgroundColor: '#5c005c',
+    padding: '0 5%',
+    boxShadow: '0 10px 30px -10px rgba(0,0,0,0.5)',
     position: 'fixed',
     zIndex: 500,
-    height: '128px',
-    fontSize: '24px',
-    paddingTop: '32px',
+    height: '150px',
+    width: '100%',
+  },
+  fixedHeading: {
+    fontFamily: 'Source Sans Pro ExtraLight',
     color: '#f8f8f8',
-*/
+    transition: 'font-size 167ms cubic-bezier(.1,.9,.2,1)',
+    fontSize: '24px',
+    paddingTop: '50px',
+  },
+  fixedLink: {
+    display: 'block',
+    textAlign: 'center',
+    paddingLeft: 0,
+    paddingTop: '14px',
+    paddingBottom: '14px',
+    paddingRight: '2vw',
+    textDecoration: 'none',
+    fontFamily: 'Source Sans Pro ExtraLight',
+    fontSize: '15px',
+    color: '#fff',
+    fontWeight: 'bold',
+    ':hover': {
+      fontFamily: 'Source Sans Pro',
+    },
+    '::after': {
+      display: 'block',
+      content: 'attr(title)',
+      fontFamily: 'Source Sans Pro',
+      fontWeight: '100',
+      height: '0',
+      overflow: 'hidden',
+      visibility: 'hidden',
+    },
+  },
+});
