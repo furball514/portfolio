54c5dcd8d2a8f312d02067ed80167ed1a38821b3
furball514 <karthik.nallasivan@gmail.com>
2017-10-04 15:18:50 +0530

    3:18

--- src/components/Sections/Showcase.js
+++ src/components/Sections/Showcase.js
@@ -35,3 +35,22 @@ const styles = StyleSheet.create({
     },
   },
 });
+
+/*magenta keywords
+
+* (iframe https://codepen.io/furball/pen/egGPeZ) Simon Game
+  One of my earliest attempts at coding. My version of the <a>Simon Game</a> is a very simple memory game, utilising HTML5, CSS3, and jQuery.
+  fa-github(https://github.com/furball514/simon-game), fa-link(https://furball514.github.io/simon-game)
+  Jan 2017 
+
+* URL Shortener (gif)
+  A link shortening service in node.js using Pug and Material UI for the front-end, Koa for the back-end, and MongoDB as database.
+  fa-github(https://github.com/furball514/url_shortener_service), fa-link(https://ee.glitch.me)
+  May 2017
+
+* (icon img || gif) TutorPug
+  A functional (mock) cross-platform 'find-a-tutor' mobile app. Uses React Native and the Expo SDK for the front-end, Koa for the back-end, and MongoDB as database.
+  fa-github(https://github.com/furball514/TutorPug), fa-link(expo.io)
+  Under development
+
+*/
